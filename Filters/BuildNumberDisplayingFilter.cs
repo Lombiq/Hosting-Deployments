c916e4c5afb1c857718babe71f3c4891bcf9d961
Benedek Farkas <benedek.farkas@lombiq.com>
2016-01-22 15:16:39 +0100

    Adding and displaying ad-hoc shape to show a placeholder for the current build number
    --HG--
    branch : dev

---
+++ Filters/BuildNumberDisplayingFilter.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Orchard;
+using Orchard.DisplayManagement;
+using Orchard.Mvc.Filters;
+using Orchard.UI.Admin;
+
+namespace Lombiq.Hosting.Deployments.Filters
+{
+    /// <summary>
+    /// Displays the BuildNumber shape in the Footer Zone of the Dashboard
+    /// that can be used to indicate the current build number.
+    /// </summary>
+    public class BuildNumberDisplayingFilter : FilterProvider, IResultFilter
+    {
+        private readonly dynamic _shapeFactory;
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+
+        public BuildNumberDisplayingFilter(IShapeFactory shapeFactory, IWorkContextAccessor workContextAccessor)
+        {
+            _shapeFactory = shapeFactory;
+            _workContextAccessor = workContextAccessor;
+        }
+
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) { }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (!AdminFilter.IsApplied(filterContext.RequestContext)) return;
+
+            var footerZone = _workContextAccessor.GetContext(filterContext).Layout.Zones["Footer"];
+            footerZone = footerZone.Add(_shapeFactory.BuildNumber());
+        }
+    }
+}
---
+++ ResourceManifest.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.UI.Resources;
+
+namespace Lombiq.Hosting.Deployments
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineStyle("Lombiq.Hosting.Deployments.BuildNumber.Admin").SetUrl("hosting-deployments-buildnumber-admin.css");
+        }
+    }
+}
